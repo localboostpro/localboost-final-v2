67f8fe7410a484e8d8970f9b1084ca8f2c37e4f5
localboostpro <julien.laurensdj@gmail.com>
2025-12-20 21:13:39 +0100

    Update plans.js

--- src/lib/plans.js
+++ src/lib/plans.js
@@ -1,10 +1,12 @@
 export const PLANS = {
   basic: {
-    label: "Starter",
+    label: "Basic",
+    price: 29,
     features: { can_access_marketing: true, max_customers: 20 }
   },
   premium: {
     label: "Premium",
+    price: 99,
     features: { can_access_marketing: true, max_customers: 1000 }
   }
 };

db79ccad9807d198f379153f6ef9a62c1a7681cc
localboostpro <julien.laurensdj@gmail.com>
2025-12-22 18:35:43 +0100

    Update Marketing.jsx

--- src/views/Marketing.jsx
+++ src/views/Marketing.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { supabase } from "../lib/supabase";
 import { generatePostContent } from "../lib/openai";
 import canvasConfetti from "canvas-confetti";

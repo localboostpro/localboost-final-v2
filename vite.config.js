8e9fecd0e29c6aeee72175d4886a12fefc7f0c21
localboostpro <julien.laurensdj@gmail.com>
2025-12-23 12:07:46 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,1 +1,17 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+export default defineConfig({
+  plugins: [react()],
+  define: {
+    'process.env': {}
+  },
+  optimizeDeps: {
+    exclude: ['lucide-react'],
+  },
+  build: {
+    commonjsOptions: {
+      transformMixedEsModules: true
+    }
+  }
+})

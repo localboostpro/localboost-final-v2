a6f778a9fc9a0e573a4eda5a1f1c01d1ffa8f8e1
localboostpro <julien.laurensdj@gmail.com>
2025-12-23 12:11:06 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,33 +2,5 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
-  define: {
-    'process.env': {},
-    global: 'globalThis'
-  },
-  resolve: {
-    alias: {
-      buffer: 'rollup-plugin-node-polyfills/polyfills/buffer-es6',
-      process: 'rollup-plugin-node-polyfills/polyfills/process-es6',
-    }
-  },
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-    esbuildOptions: {
-      define: {
-        global: 'globalThis'
-      }
-    }
-  },
-  build: {
-    commonjsOptions: {
-      transformMixedEsModules: true
-    },
-    rollupOptions: {
-      output: {
-        manualChunks: undefined
-      }
-    }
-  }
+  plugins: [react()]
 })

e6c59c37274f01bc770946b2be0cf4495d66228b
localboostpro <julien.laurensdj@gmail.com>
2025-12-24 11:57:20 +0100

    Update Profile.jsx

--- src/views/Profile.jsx
+++ src/views/Profile.jsx
@@ -104,7 +104,7 @@ export default function Profile({ profile, setProfile }) {
       const logoUrl = await uploadLogo();
 
       const { error } = await supabase
-        .from("business_profiles")
+        .from("business_profile")
         .update({
           ...formData,
           opening_hours: openingHours,
@@ -171,7 +171,7 @@ export default function Profile({ profile, setProfile }) {
     try {
       // Supprimer le profil business
       const { error: deleteError } = await supabase
-        .from("business_profiles")
+        .from("business_profile")
         .delete()
         .eq("id", profile.id);
 
